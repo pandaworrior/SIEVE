2cd1d90f5d5572bde2c9f144e91869c408692086
Cheng Li <chenglinkcs@gmail.com>
2019-04-17 11:22:52 +0800

    finished analyzing the two examples

--- src/test/staticanalysis/templatecreator/TemplateCreatorTest.java
+++ src/test/staticanalysis/templatecreator/TemplateCreatorTest.java
@@ -26,9 +26,12 @@ import java.util.Map.Entry;
 
 import staticanalysis.codeparser.CodeNodeIdentifier;
 import staticanalysis.codeparser.JavaFileParser;
+import staticanalysis.codeparser.ProjectParser;
 import staticanalysis.pathanalyzer.PathAbstractionCreator;
 import staticanalysis.pathanalyzer.PathAnalyzer;
 import staticanalysis.templatecreator.TemplateCreator;
+import util.annotationparser.Invariant;
+import util.annotationparser.InvariantParser;
 import util.annotationparser.SchemaParser;
 import staticanalysis.datastructures.controlflowgraph.CFGGraph;
 import staticanalysis.datastructures.path.PathAbstraction;
@@ -41,33 +44,82 @@ import util.debug.Debug;
  */
 public class TemplateCreatorTest {
 	
+	static String projectPath = "";
+	static String projectName = "";
+	static String schemaPath = "";
+	static String txnMustedProcessFile = "";
+	static String invariantFile = "";
+	
+	public static void config(String pName) {
+		
+		projectName = pName;
+		if(projectName.contentEquals("smallbank")) {
+			projectPath = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/src/com/oltpbenchmark/benchmarks/smallbank";
+			schemaPath = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/config/olisipo/smallbank/smallbank_olisipo.sql";
+			txnMustedProcessFile = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/config/olisipo/smallbank/smallbank_txn_mustprocess.txt";
+			invariantFile = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/config/olisipo/smallbank/smallbank_inv.txt";
+		}else if (projectName.contentEquals("seats")) {
+			projectPath = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/src/com/oltpbenchmark/benchmarks/seats";
+			schemaPath = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/config/olisipo/seats/seats_olisipo.sql";
+			txnMustedProcessFile = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/config/olisipo/seats/seats_txn_mustprocess.txt";
+			invariantFile = "/home/cheng/Code/explicit_tradeoff/code/benchmark/oltpbench/config/olisipo/seats/seats_inv.txt";
+		}else {
+			throw new RuntimeException("No support for this project");
+		}
+		
+	}
+	
+	public static void generateCode() {
+		ProjectParser pjsParser = new ProjectParser(projectPath,
+				projectName);
+		// pjsParser.addFileToFileFilter("TPCW_Database.java"); //must exclude since duplicates
+		
+		pjsParser.buildFileTree();
+		//pjsParser.printOutFileTree();
+		
+		pjsParser.obtailAllControlFlowGraphs();
+		//pjsParser.printOutAllControlFlowGraphs();
+		pjsParser.obtainBindingsForAllMethods();
+		pjsParser.resolveAllBindings();
+		//pjsParser.printOutAllControlFlowGraphs();
+		
+		PathAnalyzer.addFunctionMustBeProcessedListFromFile(txnMustedProcessFile);
+		
+		HashMap<CFGGraph<CodeNodeIdentifier, Expression>, PathAbstraction> pathAbMap = PathAbstractionCreator
+		.obtainAllPathAbstractionForWholeProject(pjsParser.getMethodCfgMapping());
+		
+		HashMap<CFGGraph<CodeNodeIdentifier, Expression>, ReducedPathAbstractionSet> cfgPathAbMapping = 
+				PathAnalyzer.obtainAllReducePathAbstractions(pathAbMap);
+		
+		//define a annotation parser
+		SchemaParser sP = new SchemaParser(schemaPath);
+		sP.parseAnnotations();
+		Invariant.setSchemaParser(sP);
+		InvariantParser invP = new InvariantParser(invariantFile);
+		
+		//get all reduced control flow graph
+		List<CFGGraph<CodeNodeIdentifier, Expression>> reducedCfgList = PathAnalyzer.obtainAllReducedCfgGraphs(cfgPathAbMapping);
+		
+		System.out.println("reduced cfg list size " + reducedCfgList.size());
+		//define a template creator
+		TemplateCreator tmpCreator = new TemplateCreator(sP,invP, projectName, reducedCfgList);
+		tmpCreator.generateCode();
+		
+	}
+	
 	/**
 	 * The main method.
 	 *
 	 * @param args the arguments
 	 */
 	public static void main(String[] args){
-		JavaFileParser jfParser = new JavaFileParser(
-				"/var/tmp/workspace/georeplication/src/test/staticanalysis/templatecreator/DatabaseQueryExample.java");
-		jfParser.parseJavaFile();
-
-		HashMap<CFGGraph<CodeNodeIdentifier, Expression>, ReducedPathAbstractionSet> cfgPathAbMapping = 
-				PathAnalyzer.obtainAllReducePathAbstractions(PathAbstractionCreator
-						.obtainAllPathAbstraction(jfParser
-								.getClassMethodCFGMappings()));
 		
-		//get all reduced control flow graph
+		if(args.length != 1) {
+			System.out.println("[Correct Usage: ] java -jar .jar projectName");
+		}
 		
-		/*List<CFGGraph<CodeNodeIdentifier, Expression>> reducedCfgList = PathAnalyzer.obtainAllReducedCfgGraphs(cfgPathAbMapping);
-		
-		//define a annotation parser
-		String fileName = "/var/tmp/workspace/georeplication/src/test/util/annotationparser/sqlSchemaTest.sql";
-		SchemaParser sP = new SchemaParser(fileName);
-		sP.parseAnnotations();
-		
-		//define a template creator
-		TemplateCreator tmpCreator = new TemplateCreator(sP, "test", reducedCfgList);
-		tmpCreator.generateCode();*/
+		config(args[0]);
+		generateCode();
 		
 	}
 
